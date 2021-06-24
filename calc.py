5ab55a96794a17240c766a34206c08260c6b27e2
kpatel04 <kinjalp0412@gmail.com>
2021-06-24 11:38:14 +0530

    Initial commit

---
+++ calc.py
@@ -0,0 +1,116 @@
+#Calculator application code
+
+from tkinter import *
+
+window = Tk()
+window.geometry("312x349")
+window.resizable(0, 0)
+window.title('Py Calculator')
+window.configure(bg='#a5b886')
+
+
+def btn_click(item):
+    global expression
+    if item in range(0, 9, 1):
+        input_current_expression.set(item)
+    expression = expression + str(item)
+    input_text.set(expression)
+
+
+def btn_clear():
+    global expression
+    expression = ""
+    input_text.set("")
+    input_current_expression.set("")
+
+
+def btn_equal():
+    global expression
+    result = str(eval(expression))
+    input_current_expression.set(result)
+    expression = ""
+
+
+expression = ""
+input_text = StringVar()
+input_current_expression = StringVar()
+
+expression_frame = Frame(window, width=200,
+                         height=25,
+                         bd=0,
+                         highlightbackground="black",
+                         highlightcolor="black",
+                         highlightthickness=0)
+expression_frame.pack(side=TOP)
+
+expression_field = Entry(expression_frame,
+                         font=('arial', 15),
+                         textvariable=input_text,
+                         width=50,
+                         bg="#F6F6F6",
+                         bd=0,
+                         justify=RIGHT)
+expression_field.grid(row=0, column=0)
+expression_field.pack(ipady=1)
+
+input_frame = Frame(window, width=312,
+                    height=50,
+                    bd=0,
+                    highlightbackground="black",
+                    highlightcolor="black",
+                    highlightthickness=0)
+input_frame.pack(side=TOP)
+
+input_field = Entry(input_frame,
+                    font=('arial', 18, 'bold'),
+                    textvariable=input_current_expression,
+                    width=50,
+                    bg="#F6F6F6",
+                    bd=0,
+                    justify=RIGHT)
+input_field.grid(row=0, column=0)
+input_field.pack(ipady=10)
+
+btns_frame = Frame(window, width=312, height=272.5, bg="grey")
+btns_frame.pack()
+
+clear = Button(btns_frame, text="C", fg="black", width=32, height=3, bd=0, bg='#F1F1F1',
+               cursor="hand2", command=lambda: btn_clear()).grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+divide = Button(btns_frame, text="/", fg="white", width=10, height=3, bd=0, bg='#656565',
+                cursor="hand2", command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
+
+seven = Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+               cursor="hand2", command=lambda: btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
+eight = Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+               cursor="hand2", command=lambda: btn_click(8)).grid(row=1, column=1, padx=1, pady=1)
+nine = Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+              cursor="hand2", command=lambda: btn_click(9)).grid(row=1, column=2, padx=1, pady=1)
+multiply = Button(btns_frame, text="*", fg="white", width=10, height=3, bd=0, bg='#656565',
+                  cursor="hand2", command=lambda: btn_click("*")).grid(row=1, column=3, padx=1, pady=1)
+
+four = Button(btns_frame, text="4", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+              cursor="hand2", command=lambda: btn_click(4)).grid(row=2, column=0, padx=1, pady=1)
+five = Button(btns_frame, text="5", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+              cursor="hand2", command=lambda: btn_click(5)).grid(row=2, column=1, padx=1, pady=1)
+six = Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+             cursor="hand2", command=lambda: btn_click(6)).grid(row=2, column=2, padx=1, pady=1)
+subtract = Button(btns_frame, text="-", fg="white", width=10, height=3, bd=0, bg='#656565',
+                  cursor="hand2", command=lambda: btn_click("-")).grid(row=2, column=3, padx=1, pady=1)
+
+one = Button(btns_frame, text="1", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+             cursor="hand2", command=lambda: btn_click(1)).grid(row=3, column=0, padx=1, pady=1)
+two = Button(btns_frame, text="2", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+             cursor="hand2", command=lambda: btn_click(2)).grid(row=3, column=1, padx=1, pady=1)
+three = Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+               cursor="hand2", command=lambda: btn_click(3)).grid(row=3, column=2, padx=1, pady=1)
+addition = Button(btns_frame, text="+", fg="white", width=10, height=3, bd=0, bg='#656565',
+                  cursor="hand2", command=lambda: btn_click("+")).grid(row=3, column=3, padx=1, pady=1)
+
+zero = Button(btns_frame, text="0", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+              cursor="hand2", command=lambda: btn_click(0)).grid(row=4, column=0, padx=1, pady=1)
+dot = Button(btns_frame, text=".", fg="black", width=10, height=3, bd=0, bg='#FBEE7C',
+             cursor="hand2", command=lambda: btn_click(".")).grid(row=4, column=1, padx=1, pady=1)
+equal = Button(btns_frame, text="=", fg="white", width=21, height=3, bd=0, bg='#3D5AFD',
+               cursor="hand2", command=lambda: btn_equal()).grid(row=4, column=2, columnspan=2, padx=1, pady=1)
+
+window.mainloop()
